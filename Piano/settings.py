381d46dc6b7ed9aafb4d9b195dd0e7aa41ac02d2
TimurGovorovskyi <tgovorovskyi@gmail.com>
2025-09-07 14:46:40 +0300

    changes

---
+++ Piano/settings.py
@@ -0,0 +1,15 @@
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 400
+
+BLACK = (0, 0, 0)
+WHITE = (200, 200, 200)
+
+KEYS = {
+    "a": "a6.mp3",
+    "b": "b6.mp3",
+    "d": "d6.mp3",
+    "f": "f6.mp3",
+    "g": "g6.mp3",
+    "e": "e6.mp3",
+    "c": "c6.mp3",
+}
